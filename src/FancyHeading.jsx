25a4825c9abae7fde9d28b877f47b91fcfa056e4
Tapas Adhikary <tapas.adhikary@gmail.com>
2024-05-15 21:07:26 +0530

    defined cn and used

--- src/FancyHeading.jsx
+++ src/FancyHeading.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import clsx from 'clsx';
-import { twMerge } from 'tailwind-merge';
+import { cn } from './lib/utils'
 
 const FancyHeading = ({isPrimary, title}) => {
   return (
     <h1
-        className={twMerge(clsx("text-center text-grey-800", {"text-3xl text-blue-500": isPrimary}))}
+        className={cn("text-center text-grey-800", {"text-3xl text-blue-500": isPrimary})}
       >{title}</h1>
   )
 }
