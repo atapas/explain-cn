e0991c0d8ddd33e0c0e152b3ef36fbbb184b9ae0
Tapas Adhikary <tapas.adhikary@gmail.com>
2024-05-15 21:02:00 +0530

    Handled with tw-merge

--- src/FancyHeading.jsx
+++ src/FancyHeading.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import clsx from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 const FancyHeading = ({isPrimary, title}) => {
   return (
     <h1
-        className={clsx("text-center text-grey-800", {"text-3xl text-blue-500": isPrimary})}
+        className={twMerge(clsx("text-center text-grey-800", {"text-3xl text-blue-500": isPrimary}))}
       >{title}</h1>
   )
 }
